a7e927df7511e8003f486988668fc26540e62a04
Rob Pilling <robpilling@gmail.com>
2015-07-09 20:18:03 +0000

    memcpy.c example

--- memcpy.c
+++ memcpy.c
@@ -1,18 +1,13 @@
-struct A
+typedef struct A {
+	char buf[32];
+} A;
+
+A f()
 {
-	int x[8];
-};
+	return (A){ .buf = 3 };
+}
 
 main()
 {
-	struct A a = { 1, 2, 3, 4, 5, 6, 7, 8 };
-	struct A b = a;
-
-	printf("%d %d %d %d %d %d %d %d\n",
-			a.x[0], a.x[1], a.x[2], a.x[3],
-			a.x[4], a.x[5], a.x[6], a.x[7]);
-
-	printf("%d %d %d %d %d %d %d %d\n",
-			b.x[0], b.x[1], b.x[2], b.x[3],
-			b.x[4], b.x[5], b.x[6], b.x[7]);
+	f();
 }
