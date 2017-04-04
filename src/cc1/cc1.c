b801fa013b2de300f0f61669498fb2fc6e6d5b47
Rob Pilling <robpilling@gmail.com>
2017-04-04 21:21:32 +0100

    Handle -g0 for turning off debug

--- src/cc1/cc1.c
+++ src/cc1/cc1.c
@@ -498,8 +498,19 @@ int main(int argc, char **argv)
 			else
 				goto usage;
 
-		}else if(!strcmp(argv[i], "-g")){
-			cc1_gdebug = 1;
+		}else if(!strncmp(argv[i], "-g", 2)){
+			switch(argv[i][2]){
+				case '0':
+					if(argv[i][3]){
+				default:
+						die("-g extra argument unexpected");
+					}
+					cc1_gdebug = 0;
+					break;
+				case '\0':
+					cc1_gdebug = 1;
+					break;
+			}
 
 		}else if(!strcmp(argv[i], "-o")){
 			if(++i == argc)
